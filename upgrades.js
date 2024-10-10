d22dc024538a1b6ee5caa620c7bb3193f45602a9
Jonas Arnold <>
2024-10-10 13:11:22 +0200

    added first version of update script, updating configuration works, updating actions does not yet

--- upgrades.js
+++ upgrades.js
@@ -6,4 +6,39 @@ export default [
 		zoneMute: true,
 		inputToZoneMute: true,
 	}),
+	function v2_1_0(context, props) {
+		let changes = {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+
+		// update config
+		if (props.config) {
+			let config = props.config
+			// ahm_type was introduced in this update, previous installations defaulted to ahm64 
+			if (config.ahm_type == undefined || config.ahm_type == '') {
+				console.log(`Updating Configuration, AHM Type was ${config.ahm_type}`)
+				config.ahm_type = 'ahm64'
+				changes.updatedConfig = config
+			}
+		}
+
+		// update actions
+		for (const action of props.actions) {
+			// replace option with old name inputChannel with new option name mute_number
+			if (action.actionId === 'mute_input' || action.actionId === 'mute_zone' || action.actionId === 'input_to_zone') {
+				if (action.options.inputChannel){
+					console.log(`Updating Configuration, Found action with old option inputChannel, converting to new mute_number`)
+
+					action.options.mute_number = action.options.inputChannel
+					delete action.options.inputChannel
+
+					changes.updatedActions.push(action)
+				}
+			}
+		}
+
+		return changes
+	},
 ]
