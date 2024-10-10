5d37ab2071a456d109582b8bd775d6757124a02c
Jonas Arnold <>
2024-10-10 21:14:02 +0200

    fixed update script

--- upgrades.js
+++ upgrades.js
@@ -18,7 +18,7 @@ export default [
 			let config = props.config
 			// ahm_type was introduced in this update, previous installations defaulted to ahm64 
 			if (config.ahm_type == undefined || config.ahm_type == '') {
-				console.log(`Updating Configuration, AHM Type was ${config.ahm_type}`)
+				console.log(`Updating Configuration, AHM Type was ${config.ahm_type}. Now set to default (ahm64).`)
 				config.ahm_type = 'ahm64'
 				changes.updatedConfig = config
 			}
@@ -28,8 +28,9 @@ export default [
 		for (const action of props.actions) {
 			// replace option with old name inputChannel with new option name mute_number
 			if (action.actionId === 'mute_input' || action.actionId === 'mute_zone' || action.actionId === 'input_to_zone') {
-				if (action.options.inputChannel){
-					console.log(`Updating Configuration, Found action with old option inputChannel, converting to new mute_number`)
+				// check if the action has the option inputChannel (by checking if property exists) 
+				if (Object.hasOwn(action.options, 'inputChannel')){
+					console.log(`Updating Configuration, Found action with old option inputChannel=${action.options.inputChannel}, converting to new mute_number`)
 
 					action.options.mute_number = action.options.inputChannel
 					delete action.options.inputChannel
